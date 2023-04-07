a763e8bd064ae1fa512f44086846840dcbfaf77a
ventuscoder <shivankcode@gmail.com>
2023-04-07 12:32:13 +0530

    Use google authentication in application

--- app.js
+++ app.js
@@ -1,14 +1,72 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const session = require('express-session')
+const mongoose = require('mongoose')
+const findOrCreate = require('mongoose-find-or-create')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+mongoose.connect(process.env.URI)
+
+const userSchema = mongoose.Schema({
+    fullName: String,
+    googleID: String,
+    todos: [{type: String}]
+})
+
+userSchema.plugin(findOrCreate)
+
+const Users = mongoose.model('User', userSchema)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    maxAge: 86400000
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done)=>done(null, user))
+passport.deserializeUser(async(user, done)=>{
+    const foundUser = await Users.find(user)
+    done(null, foundUser[0])
+})
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:8000/auth/google/callback'
+}, async (accessToken, refreshToken, profile, cb) => {
+    console.log(profile)
+
+    const foundUser = await Users.find({ googleID: profile.id })
+    console.log(foundUser)
+    if (foundUser.length == 0) {
+        const newUser = new Users({fullName: profile.displayName,googleID: profile.id})
+        await newUser.save()
+        cb(null, newUser)
+    } else {
+        cb(null, foundUser[0])
+    }
+}))
 
 
 app.get('/', (req, res) => {
     res.render('enter')
+})
+
+app.get('/auth/google', passport.authenticate('google', { scope: ["profile"] }))
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', successRedirect: '/todos' }))
+
+app.get('/todos', (req, res) => {
+    res.render('todos', {user: req.user})
 })
 
 app.listen(8000, () => {
